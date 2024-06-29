0600beeb80d5953447a2488b3a0d0a79216803e3
GlennMm <matsiweglenn@gmail.com>
2024-06-29 23:06:20 +0200

    change git terminal to use lazygit instead of gitui

--- lua/plugins/features/term.lua
+++ lua/plugins/features/term.lua
@@ -12,13 +12,21 @@ return {
   end,
   keys = {
     { "<leader>tt", "<cmd>lua require('FTerm').toggle()<CR>", desc = "Floating Term" },
-    { "<leader>tg", function()
-      local gitui = require("FTerm"):new({ cmd = "gitui", border = "single", dimensions = { height = 0.8, width = 0.8 } })
-      gitui:toggle() 
-    end, desc = "GitUI" },
-    { "<leader>tb", function()
-      local btop = require("FTerm"):new({ cmd = "btop", border = "single", dimensions = { height = 0.8, width = 0.8 } })
-      btop:toggle() 
-    end, desc = "BTop" },
+    {
+      "<leader>tg",
+      function()
+        local gitui = require("FTerm"):new({ cmd = "lazygit", border = "single", dimensions = { height = 0.8, width = 0.8 } })
+        gitui:toggle()
+      end,
+      desc = "GitUI"
+    },
+    {
+      "<leader>tb",
+      function()
+        local btop = require("FTerm"):new({ cmd = "btop", border = "single", dimensions = { height = 0.8, width = 0.8 } })
+        btop:toggle()
+      end,
+      desc = "BTop"
+    },
   }
 }
