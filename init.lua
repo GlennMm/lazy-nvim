f60b745e6cb15c99ec39be66a9cd329fd6ae1e23
GlennMm <matsiweglenn@gmail.com>
2024-06-29 23:35:29 +0200

    add event = 'VeryLazy' to most of the plugins

--- init.lua
+++ init.lua
@@ -3,5 +3,3 @@ require("manager")
 require("keymaps").setup()
 require("autocmds")
 require "current-theme"
-
-vim.notify_once("Let code like never before.", vim.log.levels.INFO, { title = "Neovim" })
